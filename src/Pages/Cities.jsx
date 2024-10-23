0f8b387793515e4a2e513e975c6b9c7394ee222b
Karit01020 <krenz06@hotmail.com>
2024-10-23 16:45:27 -0500

    intento decimo

--- src/Pages/Cities.jsx
+++ src/Pages/Cities.jsx
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ImHappy } from "react-icons/im";
 
 function Cities() {
+  const [dataCities,setDataCities] = useState( [])
+  const[search, setSearch] = useState ("")
+
+  useEffect(() => { 
+
+  const getData = async () => {
+    try {
+      let cities = []
+
+      if (search != "") {
+        cities = await fetch("http://localhost:8080/api/cities/all")
+      }
+      else{
+        cities = await fetch(`http://localhost:8080/api/cities/all?name=${search}`)
+      } 
+      cities= await cities.json()
+      setDataCities(c => c = cities)
+
+    } catch (error) {
+      console.log(error);
+      
+      
+    }
+  }
+  getData()
+
+  }, [search])
+  
+
+function handlear_search (e) {
+  setSearch(s => s = e.target.value)
+
+}
+ console.log(dataCities);
+ 
   return (
     <>
       <div className="p-4 ">
-        <h1 className="text-3xl font-bold  ">Cities</h1>
+        <h1 className="text-center text-3xl font-bold py-4 ">Cities</h1>
+        <div className="w-full flex justify-center">
+          <input type="text" onChange={(e) => handlear_search(e) }/>
+        </div>
+         { dataCities.response ? 
+        <div className=" grid grid-cols-2 md:grid-cols-4 ">
+          {dataCities.response.map(city => (
+            <div>
+              <img className="w-full h-[20vh]" src={city.photo} alt="" />
+              <p className=" absolute top-2 bg-black/50 text-white"> {city.name}</p>
+              <button className=" absolute bottom-2 p-3 ">more information </button>
+            </div>
 
-        <h2 className=" flex items-center justify-center h-screen  text-slate-950 font-serif text-4xl ">
-          THIS PAGE IS UNDER CREATION
-        </h2>
+           ))}
+        </div>:"" 
+        
+        }
       </div>
     </>
   );
